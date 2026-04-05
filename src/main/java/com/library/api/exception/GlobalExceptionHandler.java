ee4ea537e205d6fcfd23196f28477706c34d18d6
Abner1309 <soul_abner@hotmail.com>
2026-04-05 15:23:11 -0300

    feat: Exception Handler Implemented.

---
+++ src/main/java/com/library/api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.library.api.exception;
+
+import com.library.api.dto.StandardError;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(BookAlreadyRegisteredException.class)
+    public ResponseEntity<StandardError> bookAlreadyRegistered(BookAlreadyRegisteredException e, HttpServletRequest request) {
+        return buildResponse(HttpStatus.BAD_REQUEST, "Book Already Registered", e.getMessage(), request);
+    }
+
+    private ResponseEntity<StandardError> buildResponse(HttpStatus status, String error, String message, HttpServletRequest request) {
+        StandardError err = new StandardError(Instant.now(), status.value(), error, message, request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
