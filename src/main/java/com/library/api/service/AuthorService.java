1fbf943428662913b7aca934fa931d9c3f8ebf86
Abner1309 <soul_abner@hotmail.com>
2026-04-08 10:42:31 -0300

    feat: Updating the Service Layer of the "Author" Entity.

--- src/main/java/com/library/api/service/AuthorService.java
+++ src/main/java/com/library/api/service/AuthorService.java
@@ -1,5 +1,10 @@
 package com.library.api.service;
 
+import com.library.api.dto.AuthorCreateDTO;
+import com.library.api.dto.AuthorResponseDTO;
+import com.library.api.dto.AuthorUpdateDTO;
+import com.library.api.exception.AuthorAlreadyExistException;
+import com.library.api.exception.AuthorNotExistException;
 import com.library.api.model.Author;
 import com.library.api.repository.AuthorRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,41 +12,62 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service @RequiredArgsConstructor
 public class AuthorService {
     private final AuthorRepository authorRepository;
 
     @Transactional
-    public boolean saveAuthor(Author author) {
-        if (author.getName().isEmpty() || author.getNationality().isEmpty()) { return false; }
-        authorRepository.save(author);
-        return true;
+    public AuthorResponseDTO saveAuthor(AuthorCreateDTO dto) {
+        if (authorRepository.existsByNameAndNationality(dto.name(), dto.nationality())) {
+            throw new AuthorAlreadyExistException();
+        }
+        Author author = new Author(dto);
+        author = authorRepository.save(author);
+        return new AuthorResponseDTO(author);
     }
 
-    public List<Author> findAll() { return authorRepository.findAll(); }
+    public List<AuthorResponseDTO> findAll() {
+        List<Author> authors = authorRepository.findAll();
+        if (authors.isEmpty()) {
+            throw new AuthorNotExistException();
+        }
+        return authors.stream().map(AuthorResponseDTO::new).collect(Collectors.toList());
+    }
 
-    public Author findById(Long id) { return authorRepository.findById(id).orElse(null); }
+    public AuthorResponseDTO findById(Long id) {
+        Author author = authorRepository.findById(id).orElseThrow(AuthorNotExistException::new);
+        return new AuthorResponseDTO(author);
+    }
 
-    public List<Author> findByName(String name) { return authorRepository.findByName(name); }
+    public List<AuthorResponseDTO> findByName(String name) {
+        List<Author> authors = authorRepository.findByName(name);
+        if (authors.isEmpty()) {
+            throw new AuthorNotExistException();
+        }
+        return authors.stream().map(AuthorResponseDTO::new).collect(Collectors.toList());
+    }
 
-    public List<Author> findByNationality(String nationality) { return authorRepository.findByNationality(nationality); }
-
-    @Transactional
-    public boolean updateAuthor(Long id, Author updatedAuthor) {
-        Author author = authorRepository.findById(id).orElse(null);
-        if (author == null) { return false; }
-        author.setName(updatedAuthor.getName());
-        author.setNationality(updatedAuthor.getNationality());
-        authorRepository.save(author);
-        return true;
+    public List<AuthorResponseDTO> findByNationality(String nationality) {
+        List<Author> authors = authorRepository.findByNationality(nationality);
+        if (authors.isEmpty()) {
+            throw new AuthorNotExistException();
+        }
+        return authors.stream().map(AuthorResponseDTO::new).collect(Collectors.toList());
     }
 
     @Transactional
-    public boolean deleteAuthor(Long id) {
-        Author author = authorRepository.findById(id).orElse(null);
-        if (author == null) { return false; }
+    public AuthorResponseDTO updateAuthor(AuthorUpdateDTO dto) {
+        Author author = authorRepository.findById(dto.id()).orElseThrow(AuthorNotExistException::new);
+        author.updateAuthor(dto);
+        author = authorRepository.save(author);
+        return new AuthorResponseDTO(author);
+    }
+
+    @Transactional
+    public void deleteAuthor(Long id) {
+        Author author = authorRepository.findById(id).orElseThrow(AuthorNotExistException::new);
         authorRepository.delete(author);
-        return true;
     }
 }
